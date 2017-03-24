73c550da1a51b6a681fa3c8e041f726ef3acde41
Igor Volodin <postmaster@myghost.name>
2017-03-24 17:12:53 +0300

    Remove build directory

--- build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /storage/Temp/gr-srig/apps
-# Build directory: /storage/Temp/gr-srig/build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/include/srig/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /storage/Temp/gr-srig/include/srig
-# Build directory: /storage/Temp/gr-srig/build/include/srig
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
